59fe72a2df11d73ce07c5a8ea1d4e5cd9c95fb10
Chatty Fish <chattyu@outlook.com>
2023-05-30 17:40:20 -0400

    remove logs

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ def calc_corr(x,y):
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-threshold = 0.6224593312018546
+#threshold = 0.6224593312018546
+threshold = 0.5
 
 print("预热...")
 # 模拟随机事件 A 和 B
@@ -44,16 +45,16 @@ print("P(B) =", p(B))
 print("验证 p(A|B) = p(B|A)p(A) / p(B)")
 print("left =", pp(A, B))
 print("right =", pp(B, A) * p(A) / p(B))
-print("手动比对，如果 left 和 right 相差不大，则说明预热成功")
+print("手动比对，如果 left 和 right 相差不大，则说明各个函数的实现是正确")
 
 print("======== 开始 ========")
 s = 100
 correct = 0
 for i in range(s):
-    n = 100
+    n = 1000
 
-    print("计算",n,"次")    
-    print("验证 p(A|B) ∝ p(B|A)p(A)")
+    #print("计算",n,"次")    
+    #print("验证 p(A|B) ∝ p(B|A)p(A)")
 
     x1 = []
     x2 = []
@@ -66,14 +67,16 @@ for i in range(s):
     left = np.array(x1)
     right = np.array(x2)
     corr1 = calc_corr(left,right)
-    print("相关系数(-1 ~ 1):", corr1)
-    print("激活：", sigmoid(corr1))
-    if sigmoid(corr1) > threshold:
-        print("验证通过")
+    #print("相关系数(-1 ~ 1):", corr1)
+    #print("激活：", sigmoid(corr1))
+    #if sigmoid(corr1) > threshold:
+    #    print("验证通过")
+    #if corr1 > threshold:
+    #    print("验证通过")
 
 
-    print("计算",n,"次")    
-    print("验证 p(A|B) ∝ p(B|A) / p(B)")
+    #print("计算",n,"次")    
+    #print("验证 p(A|B) ∝ p(B|A) / p(B)")
 
     x1 = []
     x2 = []
@@ -86,15 +89,19 @@ for i in range(s):
     left = np.array(x1)
     right = np.array(x2)
     corr2 = calc_corr(left,right)
-    print("相关系数(-1 ~ 1):", corr2)
-    print("激活：", sigmoid(corr2))
-    if sigmoid(corr2) > threshold:
-        print("验证通过")
+    #print("相关系数(-1 ~ 1):", corr2)
+    #print("激活：", sigmoid(corr2))
+    #if sigmoid(corr2) > threshold:
+    #    print("验证通过")
+    #if corr2 > threshold:
+    #    print("验证通过")
 
-    if sigmoid(corr1) > threshold and sigmoid(corr2) > threshold:
+    #if sigmoid(corr1) > threshold and sigmoid(corr2) > threshold:
+    if corr1 > threshold and corr2 > threshold:
         correct += 1
 print("======== 结束 ========")
-print("判断标准: sigmoid(相关系数) > ", threshold)
+#print("判断标准: sigmoid(相关系数) > ", threshold)
+print("判断标准: 相关系数 > ", threshold)
 print("正确率:", correct / s)
 print("如果正确率接近1, 则说明: p(A|B) ∝ p(B|A)p(A) 和 p(A|B) ∝ p(B|A) / p(B) 是等价的")
 
